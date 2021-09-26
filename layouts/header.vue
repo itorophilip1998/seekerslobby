5f6c2cd98fd042233835cdc32ac3c176b9c0c711
itorophilip1998 <itkeyzzpianny@gmail.com>
2021-09-26 01:04:29 +0100

    fix

--- layouts/header.vue
+++ layouts/header.vue
@@ -1,6 +1,34 @@
 <template>
   <div>
-    
+    <header>
+       <nav class="navbar  navbar-expand-lg navbar-light shadow-sm bg-white fixed-top">
+      <div class="container-fluid">
+        <a class="navbar-brand font-weight-bold text-muted" href="#">SeekersLobby</a>
+        <button class="navbar-toggler d-lg-none" type="button" data-toggle="collapse" data-target="#collapsibleNavId" aria-controls="collapsibleNavId"
+            aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavId">
+          <ul class="navbar-nav ml-auto mt-2 mt-lg-0 text-left text-lg-center">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Home </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Post Jobs</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Find Jobs</a>
+            </li>
+            <li class="nav-item pr-2">
+              <a class="nav-link" href="#">Contact</a>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0 ">
+            <a href="#" class="btn btn-primary rounded-x text-white my-2 my-sm-0" type="submit">Get Started</a>
+          </form>
+        </div>
+      </div>
+    </nav>
+    </header>
   </div>
 </template>
-
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,171 @@
 <template>
   <div>
-    testing
+     <!-- header section -->
+     <div id="header-section" class="py-5 mt-5">
+       <div class="container-fluid mt-md-4">
+
+            <div class="row m-0  px-lg-4 ">
+              <div class="  col-lg-7 p-0">
+                <!-- search box -->
+                <div class="search border-primary d-none d-lg-block border bg-white rounded-x p-2  pl-3">
+                    <i class="fa fa-search text-primary" aria-hidden="true"></i>
+                  <input type="text" class="searchbox p-2 border-0 " placeholder="Job Title or Keyword">
+                <i class="fa fa-map-marker text-primary" aria-hidden="true"></i>
+                  <input type="text" class="searchbox p-2 border-0 " placeholder="Location">
+                 <button class="btn-primary btn rounded-x p-2 searchbtn">Search Jobs</button>
+                </div>
+
+                <!-- bold text -->
+                <h1 class="boldtext text-center text-lg-left pt-5">
+                  Get the best <br class="d-none d-lg-block"><strong class="text-primary">Jobs</strong> <br class="d-none d-lg-block">
+                  In every <span class="text-primary font-weight-bold">niche</span>
+                </h1>
+                <!-- paragraph -->
+                <p class="col-lg-8 p-0 text-center text-lg-left mt-3 mx-auto mx-lg-0">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet officia quaerat repellendus doloribus repudiandae ipsum. Nesciunt nulla,
+                   maxime vitae provident.</p>
+                   <img src="~assets/images/pcleft.png" alt="" class="pcleft d-none d-lg-inline">
+
+                  <!--two buttons -->
+                  <div class="headerbtn col-md-8 p-0 text-center text-lg-left mx-auto mx-lg-0">
+                    <button class="btn text-primary border border-primary bg-white shadow mr-md-2 rounded-x">Hire a Freelancer</button>
+                    <button class="btn btn-primary shadow border border-primary rounded-x">Find a Job</button>
+                  </div>
+
+
+              </div>
+              <div class=" col-lg-5 text-center">
+                 <img src="~assets/images/deskman.png" class="deskman mt-md-5 mt-2 d-none d-lg-inline  w-75" alt="">
+                 <img src="~assets/images/deskman.png" class="deskman mt-md-5 mt-3 d-none  d-md-inline d-lg-none w-50" alt="">
+                 <img src="~assets/images/deskman.png" class="deskman mt-md-5 mt-4  d-inline d-md-none w-75" alt="">
+              </div>
+            </div>
+       </div>
+     </div>
+
+     <!-- best team section -->
+   <div id="getbest-team" class="py-5">
+       <div class="container-fluid mt-md-4 ">
+
+            <div class="row m-0  px-lg-4 ">
+              <div class="  col-lg-7 p-0">
+
+                <!-- bold text -->
+                <h1 class="boldtext text-center text-white text-lg-left pt-5 ">
+                  Get the best <br class="d-none d-lg-block"><strong class="text-primary">Team</strong> <br class="d-none d-lg-block">
+                  In every <span class="text-primary font-weight-bold">niche</span>
+                </h1>
+                <!-- paragraph -->
+
+                <p class="col-lg-8 p-0 text-white text-center text-lg-left mt-3 mx-auto mx-lg-0">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet officia quaerat repellendus doloribus repudiandae ipsum. Nesciunt nulla,
+                   maxime vitae provident.</p>
+                   <img src="~assets/images/kiss.png" alt="" class="pcleft2 d-none d-lg-inline">
+
+
+
+
+              </div>
+              <div class=" col-lg-5 text-center">
+                 <img src="~assets/images/kisspng-web.png" class="deskman mt-md-5 mt-2 d-none d-lg-inline  w-75" alt="">
+                 <img src="~assets/images/kisspng-web.png" class="deskman mt-md-5 mt-3 d-none  d-md-inline d-lg-none w-50" alt="">
+                 <img src="~assets/images/kisspng-web.png" class="deskman mt-md-5 mt-4  d-inline d-md-none w-75" alt="">
+              </div>
+               <!-- search box -->
+                <div class="search border-primary d-none d-lg-block border bg-white col-7 rounded-x p-2 mt-3 pl-3">
+                    <i class="fa fa-search text-primary" aria-hidden="true"></i>
+                  <input type="text" class="searchbox p-2 border-0 " placeholder="Job Title or Keyword">
+                <i class="fa fa-map-marker text-primary" aria-hidden="true"></i>
+                  <input type="text" class="searchbox p-2 border-0 " placeholder="Location">
+                 <button class="text-primary border border-primary btn rounded-x p-2 searchbtn searchbtn2">Find a freelancer</button>
+                </div>
+            </div>
+       </div>
+     </div>
+
+     <!-- need something -->
+     <div id="needsomething">
+
+              <div class="row m-0 p-4 text-center text-lg-left">
+                  <div class="col-md-6 col-lg-4 px-lg-5 ">
+                     <div class="img text-center">
+                       <img src="~assets/images/postjob.png" alt="">
+                     </div>
+                     <p>
+                       What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has
+                     </p>
+                  </div>
+                  <div class="col-md-6 col-lg-4 px-lg-5 ">
+                     <div class="img text-center">
+                       <img src="~assets/images/choocefreelancer.png" alt="">
+                     </div>
+                     <p>
+                       What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has
+                     </p>
+                  </div>
+                  <div class="col-md-6 col-lg-4 px-lg-5 ">
+                     <div class="img text-center">
+                       <img src="~assets/images/paysafely.png" alt="">
+                     </div>
+                     <p>
+                       What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has
+                     </p>
+                  </div>
+
+                  <div class="col-lg-7 mr-lg-auto order-first order-lg-last">
+                    <h1 class="do p-0 p-md-4">
+                      Do You Need Something  Done Right?
+                      <br>
+                 <button class="btn-primary mt-3 btn rounded-x p-2 px-5">Hire a freelancer</button>
+                    </h1>
+
+                  </div>
+                  <div class="col-md-6 col-lg-4 px-lg-5  order-last">
+                     <div class="img text-center">
+                       <img src="~assets/images/custormersupport.png" alt="">
+                     </div>
+                     <p>
+                       What is Lorem Ipsum Lorem Ipsum is simply dummy text of the printing and typesetting industry Lorem Ipsum has
+                     </p>
+                  </div>
+              </div>
+     </div>
+
+     <!-- contacts/about -->
+     <div id="contact">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-7"></div>
+          <div class="col-md-4 p-2">
+            <form action="" class="border border-primary p-4 rounded-x">
+              <div class="form-group">
+                <label for="email">Email</label>
+                <input type="text" class="form-control p-3 rounded-x shadow-x" name="email" id="" aria-describedby="helpId" placeholder="">
+                <small id="helpId" class="form-text d-none text-muted">Help text</small>
+              </div>
+              <div class="form-group">
+                <label for="subject">Subject</label>
+                <input type="text" class="form-control p-3 rounded-x shadow-x" name="subject" id="" aria-describedby="helpId" placeholder="">
+                <small id="helpId" class="form-text d-none text-muted">Help text</small>
+              </div>
+              <div class="form-group">
+                <label for="message">Message</label>
+                <textarea type="text" class="form-control p-3 rounded-x shadow-x" rows="5" name="message" id="" aria-describedby="helpId" placeholder=""></textarea>
+                <small id="helpId" class="form-text d-none text-muted">Help text</small>
+              </div>
+              <div class="form-group">
+                <button class="btn btn-primary text-white rounded-x shadow">Send <i class="fa fa-paper-plane-o" aria-hidden="true"></i></button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+     </div>
+
+     <!-- ournumber -->
+     <div id="our-numbers">
+
+     </div>
   </div>
 </template>
 
@@ -9,3 +174,9 @@ export default {
 auth:false,
 }
 </script>
+<style >
+.searchbtn2{
+ background: rgba(244, 120, 6, 0.08) !important;
+}
+
+</style>
